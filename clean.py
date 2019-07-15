d2f12cb7bd089483d69e801947cafb0fae363068
“datakv” <datakv@126.com>
2019-07-16 00:44:16 +0800

    feat:add clean

---
+++ clean.py
@@ -0,0 +1,19 @@
+import os
+
+
+exist=["docs","mkdocs.yml","clean.py",".git"]
+
+list = os.listdir("./") #列出目录下的所有文件和目录
+for line in list:
+    if line in exist :
+        pass
+
+    else :
+        os.system("rm -rf " + line)
+
+exit(0)
+
+os.system("mkdocs build")
+os.system("mv site/* ./")
+
+
